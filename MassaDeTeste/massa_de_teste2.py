f06ac331ca7a2c5b72c30becb1eb94cc6ac3a1d2
AndreaBrigidio <andrea.brigidio@gmail.com>
2024-07-11 21:06:32 -0300

    Selen01

--- MassaDeTeste/massa_de_teste2.py
+++ MassaDeTeste/massa_de_teste2.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
-# Carregando o arquivo CSV em um DataFrame
 df = pd.read_csv('env\dados_ficticios.csv')
 
-#Exibindo as primeiras linhas do DataFrame
 print(df.head())
 
 filtro_idade = df['idade'] > 40
@@ -12,7 +10,6 @@ filtro_renda15000 = df['renda'] > 15000
 
 df_filtrado = df[filtro_idade & filtro_renda]
 
-# Exibindo o DataFrame filtrado
 print(df_filtrado)
 
 
