80e8de0e768a5d35e1bc93f1d8075ba8989aa9d0
AndreaBrigidio <andrea.brigidio@gmail.com>
2024-07-15 17:08:34 -0300

    desconto1

---
+++ Variaveis e funcoes basicas/Desafio01.py
@@ -0,0 +1,3 @@
+print("Hello, World!")
+
+print(145+234)
---
+++ Variaveis e funcoes basicas/Desafio02.py
@@ -0,0 +1,9 @@
+
+nome = "Paula Martins"
+mes= "Janeiro"
+valor = 500
+desconto = 10
+cupom = "PAULAÉ10"
+
+print ("Olá,",nome,"! Em",mes,"você realizou uma compra no valor de R$" ,valor, 
+"e ganhou um desconto de",desconto,"% em sua proxima compra. Use o cumpom",cupom ,".")
