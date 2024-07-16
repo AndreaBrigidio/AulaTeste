8e3eef7cc5581d7b025d88fd156e83d039203aa4
AndreaBrigidio <andrea.brigidio@gmail.com>
2024-07-16 15:25:09 -0300

    inclusao oper aritm02

--- Operadores_Aritmeticos/DesafioCaique01.py
+++ Operadores_Aritmeticos/DesafioCaique01.py
@@ -1,9 +1,30 @@
-nome = input("Digite o seu do aluno: ")
-n1 = float(input("Digite a primeira nota: "))
-n2 = float(input("Digite a segunda nota: "))
-n3 = float(input("Agora a terceira nota: "))
-n4 = float(input("Por último a quarta nota: "))
+nome = input("Por favor, digite o nome do cachorro: ")
+idade = int(input("Digite a idade do cachorro em anos: "))
 
-resultado = (n1 + n2 + n3 + n4) /4 
+idadepet = idade * 7
+print(f"A idade de {nome} em anos de cachorro é: {idadepet} anos")
 
-print(f"Olá, {nome}! A sua média é: {resultado}")
+def obter_porte_do_cachorro():
+    while True:
+        porte = input("Por favor, insira o porte do cachorro (pequeno, médio, grande): ").strip().lower()
+        if porte in ["pequeno", "médio", "medio", "grande"]:
+            
+            if porte == "medio":
+                porte = "médio"
+            return porte
+        else:
+            print("Entrada inválida. Por favor, insira uma das opções: pequeno, médio ou grande.")
+
+porte_cachorro = obter_porte_do_cachorro()
+
+lucros = {
+    "pequeno": 50 - 5,
+    "médio": 60 - 15,
+    "grande": 75 - 20
+}
+
+lucro = lucros[porte_cachorro]
+banhos = int(input(f"Digite a quantidade de banhos que {nome} tomou nos últimos 12 meses: "))
+lucrobanho = lucro * banhos
+
+print(f"Olá, {nome} tem {idadepet} anos e nos últimos 12 meses o lucro com esse animal foi de R$ {lucrobanho}.")
