4d56f5baea7bdf1ac1cd451ecd11cbc464953a9c
AndreaBrigidio <andrea.brigidio@gmail.com>
2024-07-16 15:13:12 -0300

    incluir desaf01 Oper Arit

---
+++ Operadores_Aritmeticos/DesafioCaique01.py
@@ -0,0 +1,9 @@
+nome = input("Digite o seu do aluno: ")
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+n3 = float(input("Agora a terceira nota: "))
+n4 = float(input("Por último a quarta nota: "))
+
+resultado = (n1 + n2 + n3 + n4) /4 
+
+print(f"Olá, {nome}! A sua média é: {resultado}")
