148159ad0342fecc2d6d35be316bfc0800e80f6e
AndreaBrigidio <andrea.brigidio@gmail.com>
2024-07-17 15:24:11 -0300

    alteracao academia1

--- Estruturas_Condicionais/DesafioCaique_Academia1.py
+++ Estruturas_Condicionais/DesafioCaique_Academia1.py
@@ -1,4 +1,4 @@
-dias= int(imput("Quantos dias de frequencia você tem?"))
+dias = int (input('Quantos dias de frequencia você tem?: '))
 
 if dias == 21:
     print('UHUU. AGORA VOCÊ PODE PRESENTEAR UM AMIGO OU AMIGA PARA TREINAR COM VOCÊ')
@@ -7,9 +7,9 @@ elif dias <= 21 and dias > 1:
     print('VOCÊ ESTÁ PARTICIPANDO DA NOSSA PROMO TREINA JUNTO')
 
 else:
-    frequencia (input('você faltou ontem [y/n]: '))
+    frequencia = (input('Você faltou ontem [y/n]: '))
 
-if frequencia 'y':
+if frequencia == 'y':
     print('QUE BOM VER VOCÊ DE VOLTA. A PARTIR DE AGORA INICIAMOS MAIS UMA CONTAGEM DE 21 DIAS PARA A PROMO TREINA JUNTO')
 
 elif frequencia == 'n': print('VOCÊ ESTÁ PARTICIPANDO DA NOSSA PROMO TREINA JUNTO')
